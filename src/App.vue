3619d86cb2d764b673f3e053cd83496c5b1fbaf2
adaephonben <ma18btech11011@iith.ac.in>
2019-03-05 01:12:56 +0530

    Basic footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="content">
   <b-navbar toggleable="lg" type="dark" variant="info" sticky="true">
     <b-navbar-brand href="http://evidyadaan.netlify.com">eVidyadaan</b-navbar-brand>
     <b-navbar-toggle target="nav_collapse" />
@@ -28,8 +29,14 @@
     <b-button  v-on:click="iframesrc=ppt.vlink; downloadsrc=ppt.dlink" variant="outline-primary" :href="'https://docs.google.com/presentation/d/'+downloadsrc+'/export/pdf'">Download this presentation</b-button>
   </div>
   <br />
-  
+  <br />
+  <br />
+</div>
+  <footer class="page-footer">
+    Site developed for <a href="http://nss.iith.ac.in" class="footer-link">NSS, IIT Hyderabad</a> by <a href="https://github.com/adaephonben" class="footer-link">AdaephonBen</a>
+  </footer>
   </div>
+
 </template>
 
 <script>
@@ -79,5 +86,32 @@ export default {
     width: 100%;
     height: 100%;
     border: 0;
+}
+
+.page-footer {
+  padding-top : 5px;
+  width : 100%;
+  bottom: 0;
+  position: fixed;
+  height : 35px;
+  text-align: left;
+  padding-left: 10px ; 
+  background-color: #17A2B8 ;
+  color: #FFFCEB;
+}
+.footer-link {
+  border-bottom: 1px solid #453886;
+  color: #453886;
+  padding-bottom: .25em;
+  text-decoration: none;
+}
+.footer-link:hover {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg id='squiggle-link' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:ev='http://www.w3.org/2001/xml-events' viewBox='0 0 20 4'%3E%3Cstyle type='text/css'%3E.squiggle{animation:shift .3s linear infinite;}@keyframes shift {from {transform:translateX(0);}to {transform:translateX(-20px);}}%3C/style%3E%3Cpath fill='none' stroke='%23453886' stroke-width='2' class='squiggle' d='M0,3.5 c 5,0,5,-3,10,-3 s 5,3,10,3 c 5,0,5,-3,10,-3 s 5,3,10,3'/%3E%3C/svg%3E");
+  background-position: bottom;
+  background-repeat: repeat-x;
+  background-size: 20%;
+  border-bottom: 0;
+  padding-bottom: .3em;
+  text-decoration: none;
 }
 </style>
